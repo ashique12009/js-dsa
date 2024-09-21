bcf137f1782dc1e138f3685826cdb50a26bc5dbd
ashique <ashique12009@gmail.com>
2024-09-21 20:13:46 +0300

    remove unused code

--- character-count.js
+++ character-count.js
@@ -1,5 +1,18 @@
 function characterCount(str) {
-    
+    let charList = {}
+
+    for (let index = 0; index < str.length; index++) {
+
+        let char = str[index]
+
+        if (charList[char]) {
+            charList[char]++
+        } else {
+            charList[char] = 1
+        }
+    }
+
+    return charList
 }
 
-characterCount("hello world");
+console.log(characterCount("hhllh world"));
