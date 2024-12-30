d20361999c8a23a84845f3f04861d40568a65f8a
ashique <ashique12009@gmail.com>
2024-12-30 12:05:56 +0200

    linked list with add method

---
+++ linked-list.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(value) {
+        let newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) 
+                current = current.next;
+            current.next = newNode;
+        }
+    }
+
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+}
+
+let list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.print(); // Output: 1 2 3
